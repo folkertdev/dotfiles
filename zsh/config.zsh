18d2f71f3c1d43b7b8cf89e4954ce7faf2081143
Folkert de Vries <flokkievids@gmail.com>
2016-02-15 18:00:02 +0100

    delete some files

--- zsh/config.zsh
+++
@@ -1,8 +0,0 @@
-#make terminal command navigation sane again
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
-bindkey '^?' backward-delete-char
--- zsh/git.zsh
+++
@@ -1,7 +0,0 @@
-# aliases
-alias git-undo='git reset --soft HEAD~1'
-alias git-list='git branch --list'
-
-alias diff-vs-remote='git diff origin/master --'
-
-
